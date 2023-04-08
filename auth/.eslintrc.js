32a47b4ca24c3c9bb8211ec1a5c95f9d2d0d2d6b
Martin <persky03@gmail.com>
2023-04-08 23:38:34 +0200

    build: prettier and more

---
+++ auth/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["import", "prettier", "@typescript-eslint"],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  rules: {
+    quotes: [
+      "error",
+      "single",
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-underscore-dangle": ["error", { allow: ["_id", "_update"] }],
+    "class-methods-use-this": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+};
